bfa861215fa0d370a843865877cdcfadf0167b98
sakiv93 <vikas.sakiv93@gmail.com>
2019-11-06 14:33:52 +0100

    Program for general matrix is done of any size, Have to do error handling and increase the size allocation for matrices

--- Exercise1.c
+++ Exercise1.c
@@ -2,38 +2,46 @@
 int main()
 {
     int matrix_a[3][3], matrix_b[3][3], matrix_m[3][3];
-    int i,j,k, sum;
+    int i,j,k, sum,a,b,c,d;
+    printf("Enter the number of rows of matrix_a:\n");
+    scanf("%d",&a);
+    printf("Enter the number of coloums of matrix_a:\n");
+    scanf("%d",&b);
 /* ############################################################ */
     printf("Enter the values of matrix_a:\n");
-    for (i=1;i<4;i++)
+    for (i=1;i<(a+1);i++)
     {
-        for (j=1;j<4;j++)
+        for (j=1;j<(b+1);j++)
         {
             scanf("%d",&matrix_a[i][j]);
         }
     }
     printf("matrix_a is:\n");
-    for (i=1;i<4;i++)
+    for (i=1;i<(a+1);i++)
     {
-        for (j=1;j<4;j++)
+        for (j=1;j<(b+1);j++)
         {
             printf("%d\t",matrix_a[i][j]);
         }
         printf("\n");
     }
 /* ############################################################ */
+    printf("Enter the number of rows of matrix_b:\n");
+    scanf("%d",&c);
+    printf("Enter the number of coloums of matrix_b:\n");
+    scanf("%d",&d);
     printf("Enter the values of matrix_b:\n");
-    for (i=1;i<4;i++)
+    for (i=1;i<(c+1);i++)
     {
-        for (j=1;j<4;j++)
+        for (j=1;j<(d+1);j++)
         {
             scanf("%d",&matrix_b[i][j]);
         }
     }
     printf("matrix_b is:\n");
-    for (i=1;i<4;i++)
+    for (i=1;i<(c+1);i++)
     {
-        for (j=1;j<4;j++)
+        for (j=1;j<(d+1);j++)
         {
             printf("%d\t",matrix_b[i][j]);
         }
@@ -55,9 +63,9 @@ int main()
 
     }
     printf("matrix_m is:\n");
-    for (i=1;i<4;i++)
+    for (i=1;i<(a+1);i++)
     {
-        for (j=1;j<4;j++)
+        for (j=1;j<(d+1);j++)
         {
             printf("%d\t",matrix_m[i][j]);
         }
